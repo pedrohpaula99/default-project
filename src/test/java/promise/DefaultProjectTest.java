40658f7ae907d7a715017b8a36b13586f55d7ac7
pedrohpaula99 <pedrohpaula99>
2023-06-15 03:29:49 +0000

    Initial commit

---
+++ src/test/java/promise/DefaultProjectTest.java
@@ -0,0 +1,21 @@
+package promise;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class DefaultProjectTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
